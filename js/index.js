e453834c0bdf7929a8767cf4ed8a77f6975b9ec2
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2019-05-03 19:13:32 -0400

    mouseenter and mouseleave on busimg

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+let busPicture = document.querySelector('img');
+console.log(busPicture);
+
+busPicture.addEventListener('mouseenter', function(fadeOut) {
+  fadeOut.target.style.opacity = '0.4';
+});
+
+busPicture.addEventListener('mouseleave', function(fadeIn) {
+  fadeIn.target.style.opacity = '1';
+});
