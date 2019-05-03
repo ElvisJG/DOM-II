9188664154cb607467e172e49c1a7fd2a1bdd946
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2019-05-03 19:42:01 -0400

    added some more.. stuff

--- js/index.js
+++ js/index.js
@@ -2,10 +2,22 @@
 let busPicture = document.querySelector('img');
 console.log(busPicture);
 
-busPicture.addEventListener('mouseenter', function(fadeOut) {
-  fadeOut.target.style.opacity = '0.4';
+// 1
+busPicture.addEventListener('mouseenter', function(sepiaOn) {
+  sepiaOn.target.style.filter = 'sepia(30%)';
+});
+// 2
+busPicture.addEventListener('mouseleave', function(sepiaOff) {
+  sepiaOff.target.style.filter = 'sepia(0%)';
 });
 
-busPicture.addEventListener('mouseleave', function(fadeIn) {
-  fadeIn.target.style.opacity = '1';
+let funBusH1 = document.querySelector('h1');
+console.log(funBusH1);
+// 3
+funBusH1.addEventListener('click', event => {
+  funBusH1.innerHTML = 'Yikes';
+});
+// 4
+funBusH1.addEventListener('dblclick', event => {
+  funBusH1.innerHTML = 'Fun Bus';
 });
